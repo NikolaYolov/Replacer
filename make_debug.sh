7973e345c9e6a03d3a8ec4126938e89f44091d56
NikolaYolov <nikolayolov@gmail.com>
2011-06-23 00:18:13 +0300

    fixed a bug related with string allocation and added build scripts

---
+++ make_debug.sh
@@ -0,0 +1,1 @@
+gcc -std=c99 -Wall -Wextra -Werror -g translate.c s_alloc.c aut_table.c main.c -o replacer
---
+++ make_release.sh
@@ -0,0 +1,1 @@
+gcc -std=c99 -Wall -Wextra -Werror -O3 -DNDEBUG translate.c s_alloc.c aut_table.c main.c -o replacer
